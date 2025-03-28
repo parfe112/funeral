1adc4052759c45fe81c2a2f36927eeeaa54e4db0
parfe112 <iamrebel112@yahoo.com>
2025-03-28 21:33:44 +0200

    asasa

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,16 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 
+import node from "@astrojs/node";
+
 export default defineConfig({
   vite: {
     plugins: [
       tailwindcss()
     ],
   },
+
+  adapter: node({
+    mode: "standalone",
+  }),
 });
